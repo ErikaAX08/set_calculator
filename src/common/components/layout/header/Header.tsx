02868ecff247100fd1d49f6222dfd0b08c4decb6
Erika AX <eamastalxochimitl@gmail.com>
2024-01-19 17:22:32 -0600

    Calculator expand and collapse button added

--- src/common/components/layout/header/Header.tsx
+++ src/common/components/layout/header/Header.tsx
@@ -1,11 +1,29 @@
-import styles from "./Header.module.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
 
-function Header() {
-  return (
-    <header>
-      <h1 className={styles.title} >Sets Calculator</h1>
-    </header>
-  )
+import styles from "./Header.module.css";
+
+interface HeaderProps {
+  onHandleMenuToggle: () => void;
+  isMenuOpen: boolean;
 }
 
-export default Header
+const Header: React.FC<HeaderProps> = ({ onHandleMenuToggle, isMenuOpen }) => {
+  const handleIconClick = () => {
+    onHandleMenuToggle();
+  };
+
+  return (
+    <header className={styles.container}>
+      <h1 className={styles.title}>Sets Calculator</h1>
+      <FontAwesomeIcon
+        className={`${styles.icon} ${isMenuOpen ? styles.icom_rotate : ""}`}
+        icon={faChevronDown}
+        onClick={handleIconClick}
+      />
+    </header>
+  );
+};
+
+export default Header;

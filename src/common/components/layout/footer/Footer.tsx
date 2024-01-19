d840e196e0ee9c54fd44904be80d34cbddcce3ce
Erika AX <eamastalxochimitl@gmail.com>
2024-01-19 17:24:44 -0600

    Style to hide footer added

--- src/common/components/layout/footer/Footer.tsx
+++ src/common/components/layout/footer/Footer.tsx
@@ -1,8 +1,13 @@
+import React from "react"
 import styles from "./Footer.module.css"
 
-function Footer() {
+interface FooterProps {
+  isMenuOpen: boolean
+}
+
+const Footer: React.FC<FooterProps> = ({ isMenuOpen }) => {
   return (
-    <footer className={styles.footer}>
+    <footer className={`${styles.footer} ${isMenuOpen ? styles.footer_hide : ""}`}>
         <p>Made by <a className={styles.link} href="https://github.com/ErikaAX08" target="_blank">@ErikaAX</a> </p>
     </footer>
   )

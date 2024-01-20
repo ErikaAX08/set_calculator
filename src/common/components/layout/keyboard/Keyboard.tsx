ef62b77a8e7435b4dc94d96357d2347c885c2867
Erika AX <eamastalxochimitl@gmail.com>
2024-01-19 18:30:00 -0600

    Updated calculator UI

--- src/common/components/layout/keyboard/Keyboard.tsx
+++ src/common/components/layout/keyboard/Keyboard.tsx
@@ -1,88 +1,89 @@
 import styles from "./Keyboard.module.css";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 interface KeyboardProps {
-  onCalculate: (operation: number) => void;
-  resetSets: () => void
+  isMenuOpen: boolean;
 }
 
-const Keyboard: React.FC<KeyboardProps> = ({ onCalculate, resetSets }) => {
+const Keyboard: React.FC<KeyboardProps> = ({ isMenuOpen }) => {
   const [selectedOperation, setSelectedOperation] = useState<number>(0);
 
   const handleOperationClick = (operation: number) => {
     setSelectedOperation(operation);
-    onCalculate(selectedOperation)
   };
-  
-  useEffect(() => {
-    onCalculate(selectedOperation);
-  }, [selectedOperation, onCalculate]);
-
-  const handleResetClick = () => {
-    resetSets()
-  };
-
+ 
   return (
-    <div>
-      <div className={styles.container}>
+    <div className={ `${styles.container} ${ isMenuOpen ? styles.container_hidde : "" }` } >
+      <form className={ styles.inputContainer }>
+        <input className={ styles.input } placeholder="" />
+        <span className={ styles.calculatorMessage } >Error performing operation</span>
+      </form>
+      <div className={styles.buttonsContainer}>
         <div
           className={`${styles.buttonOption} ${
             selectedOperation == 0 ? styles.selectedOption : ""
           }`}
-          onClick={() => handleOperationClick(0)}
         >
-          <p className={styles.mathSymboltText}>A = B</p>
-          <span className={styles.buttonSubtitle}>Compare</span>
+          <p className={styles.mathSymboltText}>P(x)</p>
         </div>
         <div
           className={`${styles.buttonOption} ${
             selectedOperation == 1 ? styles.selectedOption : ""
           }`}
-          onClick={() => handleOperationClick(1)}
         >
-          <p className={styles.mathSymboltText}>
-            2
-            <sup>
-              <span className={styles.mathSymboltTextSub}>A</span>
-            </sup>
-          </p>
-          <span className={styles.buttonSubtitle}>Power Set</span>
+          <p className={styles.mathSymboltText}>(x)</p>
         </div>
         <div
           className={`${styles.buttonOption} ${
             selectedOperation == 2 ? styles.selectedOption : ""
           }`}
-          onClick={() => handleOperationClick(2)}
         >
-          <p className={styles.mathSymboltText}>A ∩ B</p>
-          <span className={styles.buttonSubtitle}>Intersection</span>
+          <p className={styles.mathSymboltText}>C</p>
         </div>
         <div
           className={`${styles.buttonOption} ${
             selectedOperation == 3 ? styles.selectedOption : ""
           }`}
-          onClick={() => handleOperationClick(3)}
         >
-          <p className={styles.mathSymboltText}>A ∪ B</p>
-          <span className={styles.buttonSubtitle}>Union</span>
+          <p className={styles.mathSymboltText}>∪</p>
         </div>
         <div
           className={`${styles.buttonOption} ${
             selectedOperation == 4 ? styles.selectedOption : ""
           }`}
-          onClick={() => handleOperationClick(4)}
         >
-          <p className={styles.mathSymboltText}>A - B</p>
-          <span className={styles.buttonSubtitle}>Relative Complement</span>
+          <p className={styles.mathSymboltText}>∩</p>
+        </div>
+        <div
+          className={`${styles.buttonOption} ${
+            selectedOperation == 5 ? styles.selectedOption : ""
+          }`}
+        >
+          <p className={styles.mathSymboltText}>/</p>
+        </div>
+        <div
+          className={`${styles.buttonOption} ${
+            selectedOperation == 6 ? styles.selectedOption : ""
+          }`}
+        >
+          <p className={styles.mathSymboltText}>A′</p>
+        </div>
+        <div
+          className={`${styles.buttonOption} ${
+            selectedOperation == 7 ? styles.selectedOption : ""
+          }`}
+        >
+          <p className={styles.mathSymboltText}>Δ</p>
+        </div>
+        <div
+          className={`${styles.buttonOption} ${
+            selectedOperation == 8 ? styles.selectedOption : ""
+          }`}
+        >
+          <p className={styles.mathSymboltText}>=</p>
         </div>
       </div>
-      <button
-        className={styles.buttonCalculate}
-        onClick={() => handleResetClick()}
-      >
-        Reset
-      </button>
     </div>
   );
 };

089fc802eb354189b65e35f7d3f34ef697f2dd6f
Erika AX <eamastalxochimitl@gmail.com>
2024-01-20 16:37:17 -0600

    Universal set, some placeholders and type set added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,96 @@
 "use client";
-import { Footer, Header, Keyboard, Input, Results } from "@components/layout";
 import styles from "./page.module.css";
+import { Footer, Header, Keyboard, Input, Results } from "@components/layout";
+import { handleOperation } from "@hooks/index";
+import { cleanSet } from "@components/utils";
+import { Set } from "@components/set";
 
 import { useState, useEffect } from "react";
-import { handleOperation } from "@hooks/index";
 
 export default function Home() {
-  const [sets, setSets] = useState<string[]>([]);
+  const [sets, setSets] = useState<Set[]>([]);
+  const [universalSet, setUniversalSet] = useState<Set | null>(null);
   const [selectedOperation, setSelectedOperation] = useState<number>(0);
-  const [letters, setLetters] = useState<string[]>(["A"] as string[]);
+  const [letters, setLetters] = useState<string[]>([]);
   const [result, setResult] = useState<string[]>([]);
-  
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const placeholders = ["Enter the universal set", "Enter a set"];
+
   const handleAddSet = (newSet: string) => {
-    setSets([...sets, newSet]);
+    if (universalSet === null) setUniversalSet(cleanSet(newSet));
+    
+    else {
+      const newSetResult: Set = cleanSet(newSet);
+      let isSubset: boolean = true;
+      
+      newSetResult.elements.map((item: string) => {
+        if (!universalSet.elements.includes(item)) {
+          isSubset = false;
+        }
+      });
+    
+      if (isSubset) {
+        setSets([...sets, newSetResult]);
+      }
+      else setResult([...result, "The set entered is not a subset of U"]);
+    }
   };
 
-  const handleCalculate = (operation: number) => {
-    setSelectedOperation(operation);    
-  };
-  
   useEffect(() => {
     setLetters((prevLetters) => {
-      const lastLetter = prevLetters[prevLetters.length - 1];
-      const newLetter = String.fromCharCode(lastLetter.charCodeAt(0) + 1);
+      let newLetter = "A";
+
+      if (prevLetters.length > 0) {
+        const lastLetter = prevLetters[prevLetters.length - 1];
+        newLetter = String.fromCharCode(lastLetter.charCodeAt(0) + 1);
+      }
+
       return [...prevLetters, newLetter];
     });
   }, [sets]);
-  
+
   useEffect(() => {
     if (sets.length >= 2) {
       setResult(handleOperation(sets, letters, selectedOperation));
     }
   }, [selectedOperation, letters, sets]);
-  
-  const resetSets = () => {
-    setSets([]);
-    setLetters(["A"]);
-    setResult([]);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      const screenSizeThreshold = 767;
+      setIsMenuOpen(window.innerWidth <= screenSizeThreshold);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <main className={styles.main}>
       <div className={styles.contentContainer}>
         <div>
-          <Results sets={sets} result={result} letters={letters} />
-          <Input onAddSet={handleAddSet} />
+          <Results
+            universalSet={universalSet}
+            sets={sets}
+            result={result}
+            letters={letters}
+          />
+          <Input onAddSet={handleAddSet} placeholders={placeholders} />
         </div>
       </div>
 
       <div className={styles.optionsContainer}>
-        <Header />
-        <Keyboard onCalculate={handleCalculate} resetSets={resetSets} />
-        <Footer />
+        <Header onHandleMenuToggle={handleMenuToggle} isMenuOpen={isMenuOpen} />
+        <Keyboard isMenuOpen={isMenuOpen} />
+        <Footer isMenuOpen={isMenuOpen} />
       </div>
     </main>
   );

262ccd16739b5f4a47a33a65c9dea684484f5254
Erika AX <eamastalxochimitl@gmail.com>
2024-01-25 20:47:41 -0600

    Forming the operation with the value of each button, also eliminating characters and controls from the cursor

--- src/common/components/layout/keyboard/Keyboard.tsx
+++ src/common/components/layout/keyboard/Keyboard.tsx
@@ -8,7 +8,7 @@ import {
   faEquals,
 } from "@fortawesome/free-solid-svg-icons";
 
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState, useRef } from "react";
 
 interface KeyboardProps {
   isMenuOpen: boolean;
@@ -22,12 +22,19 @@ const Keyboard: React.FC<KeyboardProps> = ({
   letters,
 }) => {
   const [isInputDisabled, setIsInputDisabled] = useState(false);
+  const [operation, setOperation] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+  const [cursorPosition, setCursorPosition] = useState<number | null>(null);
 
   useEffect(() => {
     const handleResize = () => {
       const maxWidthForDisabledInput = 1024;
       const currentWidth = window.innerWidth;
       setIsInputDisabled(currentWidth < maxWidthForDisabledInput);
+
+      if (currentWidth < maxWidthForDisabledInput) {
+        setCursorPosition(null);
+      }
     };
 
     window.addEventListener("resize", handleResize);
@@ -39,6 +46,65 @@ const Keyboard: React.FC<KeyboardProps> = ({
     };
   }, []);
 
+  const handleButtonClick = (symbol: string) => {
+    let newOperation;
+    let positionToUse;
+
+    if (symbol === "delete") {
+      positionToUse =
+        cursorPosition !== null && cursorPosition > 0
+          ? cursorPosition - 1
+          : operation.length - 1;
+
+      newOperation =
+        operation.slice(0, positionToUse) + operation.slice(positionToUse + 1);
+      setCursorPosition(positionToUse);
+
+      if (operation.length <= 0) setCursorPosition(0);
+    } else {
+      positionToUse =
+        cursorPosition !== null ? cursorPosition : operation.length;
+      newOperation =
+        operation.slice(0, positionToUse) +
+        symbol +
+        operation.slice(positionToUse);
+      setCursorPosition(positionToUse + 1);
+    }
+
+    setOperation(newOperation);
+    setError(null);
+  };
+
+  const handleInputClick = (event: React.MouseEvent<HTMLInputElement>) => {
+    const input = event.currentTarget;
+    setCursorPosition(input.selectionStart);
+  };
+
+  const handleClearOperation = () => {
+    setOperation("");
+    setCursorPosition(0);
+  };
+
+  const handleCalculate = () => {
+    console.log(`Calcular operacion ${operation}`);
+  };
+
+  const handleMoveToLeft = () => {
+    let positionToUse =
+      cursorPosition !== null && cursorPosition > 0 ? cursorPosition - 1 : 0;
+
+    setCursorPosition(positionToUse);
+  };
+
+  const handleMoveToRight = () => {
+    let positionToUse =
+      cursorPosition !== null && cursorPosition < operation.length
+        ? cursorPosition + 1
+        : operation.length;
+
+    setCursorPosition(positionToUse);
+  };
+
   return (
     <div
       className={`${styles.container} ${
@@ -46,59 +112,106 @@ const Keyboard: React.FC<KeyboardProps> = ({
       }`}
     >
       <form className={styles.inputContainer}>
-        <input
-          className={styles.input}
-          placeholder=""
-          disabled={isInputDisabled}
-        />
-        <span className={styles.calculatorMessage}>
-          Error performing operation
-        </span>
+        <div className={styles.inputWrapper}>
+          <input
+            className={styles.input}
+            placeholder=""
+            disabled={isInputDisabled}
+            value={operation}
+            onChange={(e) => {
+              setOperation(e.target.value);
+              setCursorPosition(e.target.selectionStart || 0);
+            }}
+            onClick={handleInputClick}
+          />
+          <div
+            className={styles.cursor}
+            style={{ left: `${cursorPosition}ch` }}
+          />
+        </div>
+        {error !== null && (
+          <span className={styles.calculatorMessage}>
+            Error performing operation
+          </span>
+        )}
       </form>
       <div className={styles.buttonsContainer}>
-        <div className={styles.buttonOption}>
+        <div
+          className={styles.buttonOption}
+          onClick={() => handleButtonClick("P()")}
+        >
           <p className={styles.mathSymboltText}>P(x)</p>
         </div>
-        <div className={styles.buttonOption}>
+        <div
+          className={styles.buttonOption}
+          onClick={() => handleButtonClick("()")}
+        >
           <p className={styles.mathSymboltText}>(x)</p>
         </div>
-        <div className={`${styles.buttonOption} ${styles.buttonDelete}`}>
+        <div
+          className={`${styles.buttonOption} ${styles.buttonDelete}`}
+          onClick={() => handleClearOperation()}
+        >
           <FontAwesomeIcon icon={faC} className={styles.mathSymboltText} />
         </div>
-        <div className={styles.buttonOption}>
+        <div
+          className={styles.buttonOption}
+          onClick={() => handleButtonClick("∪")}
+        >
           <p className={styles.mathSymboltText}>∪</p>
         </div>
-        <div className={styles.buttonOption}>
+        <div
+          className={styles.buttonOption}
+          onClick={() => handleButtonClick("∩")}
+        >
           <p className={styles.mathSymboltText}>∩</p>
         </div>
-        <div className={styles.buttonOption}>
+        <div
+          className={styles.buttonOption}
+          onClick={() => handleButtonClick("/")}
+        >
           <p className={styles.mathSymboltText}>/</p>
         </div>
-        <div className={styles.buttonOption}>
+        <div
+          className={styles.buttonOption}
+          onClick={() => handleButtonClick("A′")}
+        >
           <p className={styles.mathSymboltText}>A′</p>
         </div>
-        <div className={styles.buttonOption}>
+        <div
+          className={styles.buttonOption}
+          onClick={() => handleButtonClick("Δ")}
+        >
           <p className={styles.mathSymboltText}>Δ</p>
         </div>
-        <div className={styles.buttonOption}>
+        <div
+          className={styles.buttonOption}
+          onClick={() => handleButtonClick("delete")}
+        >
           <FontAwesomeIcon
             icon={faDeleteLeft}
             className={styles.mathSymboltText}
           />
         </div>
-        <div className={styles.buttonOption}>
+        <div className={styles.buttonOption} onClick={() => handleMoveToLeft()}>
           <FontAwesomeIcon
             icon={faChevronLeft}
             className={styles.mathSymboltText}
           />
         </div>
-        <div className={styles.buttonOption}>
+        <div
+          className={styles.buttonOption}
+          onClick={() => handleMoveToRight()}
+        >
           <FontAwesomeIcon
             icon={faChevronRight}
             className={styles.mathSymboltText}
           />
         </div>
-        <div className={`${styles.buttonOption} ${styles.buttonAccent}`}>
+        <div
+          className={`${styles.buttonOption} ${styles.buttonAccent}`}
+          onClick={() => handleCalculate()}
+        >
           <FontAwesomeIcon icon={faEquals} className={styles.mathSymboltText} />
         </div>
       </div>
@@ -107,12 +220,19 @@ const Keyboard: React.FC<KeyboardProps> = ({
           <h4>Available sets</h4>
           <div className={styles.setsContainer}>
             {isUniversalSet && (
-              <div className={`${styles.buttonSet} ${styles.buttonAccent}`}>
+              <div
+                className={`${styles.buttonSet} ${styles.buttonAccent}`}
+                onClick={() => handleButtonClick("U")}
+              >
                 <p className={styles.mathSymboltText}>U</p>
               </div>
             )}
             {letters.map((letter, index) => (
-              <div key={index} className={`${styles.buttonSet}`}>
+              <div
+                key={index}
+                className={`${styles.buttonSet}`}
+                onClick={() => handleButtonClick(letter)}
+              >
                 <p className={styles.mathSymboltText}>{letter}</p>
               </div>
             ))}
